d6fdd2d4d962836b48c57690e2cbf30cdad903f6
evridiki91 <ec15441@my.bristol.ac.uk>
2018-03-17 21:46:53 +0000

    Implementing part 2- Identity crisis

--- assignment2_wp_12345.pl
+++ assignment2_wp_12345.pl
@@ -7,8 +7,29 @@ find_identity(A):-
   ; otherwise -> find_identity_o(A)
   ).
 
-find_identity_2(A):-
-  A='Not yet implemented'.
+generate_actor_link_list(X):-
+  bagof([actor(A)|L], (actor(A), generate_actor_link(A,L)), X).
 
+generate_actor_link(A,Ls):-
+  bagof(L, (wp(A,WT),wt_link(WT,L)), Ls ).
+
+find_identity_2(A):-
+  generate_actor_link_list(List),
+  whatsmyname(A,List).
+
+found_myself(A,List):-
+  List = [[ actor(A)| Links]].
+
+whatsmyname(A,List):-
+  found_myself(A,List).
+
+whatsmyname(A,List):-
+  agent_ask_oracle(oscar,o(1),link,L),
+  filter_actors(List,L,FilteredList),
+  whatsmyname(A,FilteredList).
+
+filter_actors(List,Link,FilteredList):-
+  List = [[Actor(A)|Links] | RestActors],
+  
 find_identity_o(A):-
   A='Not yet implemented'.
