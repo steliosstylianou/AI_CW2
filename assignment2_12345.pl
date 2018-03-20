4ed17715997373dd0f92adbd85a5301e7840ee83
Stelios Stylianou <stylianoustelioscy@gmail.com>
2018-03-20 22:44:46 +0000

    still not working

--- assignment2_12345.pl
+++ assignment2_12345.pl
@@ -4,7 +4,7 @@ candidate_number(12345).
 solve_task(Task,Cost):-
   my_agent(Agent),
   query_world( agent_current_position, [Agent,P] ),
-  solve_task_astar(Task,[[c(0,0,P),P]],0,R,Cost,_NewPos),!,  % prune choice point for efficiency
+  solve_task_astar(Task,[[c(0,0,P),P]],0,R,Cost,_NewPos,[]),!,  % prune choice point for efficiency
   reverse(R,[_Init|Path]),
   query_world( agent_do_moves, [Agent,Path] ).
 
@@ -37,14 +37,14 @@ search(F,N,N,1) :-
   %%%%%%%%%% A-STAR SEARCH %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % agenda is a list of current structures
 
-solve_task_astar(Task,Agenda,D,RPath,[cost(C),depth(G)],NewPos) :-
+solve_task_astar(Task,Agenda,D,RPath,[cost(C),depth(G)],NewPos,Visited) :-
   %Parsing agenda to get current
   % writeln('Inside astar achieve clause'),
   Agenda =  [[c(F,G,Pos)|RPath]|Rest],
   Current = [c(F,G,Pos)|RPath],
   achieved_v2(Task,Current,RPath,C,NewPos).
 
-solve_task_astar(Task,Agenda,D,RR,Cost,NewPos) :-
+solve_task_astar(Task,Agenda,D,RR,Cost,NewPos,Visited) :-
   Task = go(P),
   % writeln('Solve astar'),
   Agenda =  [[c(F,G,Pos)|RPath]|Rest],
@@ -55,9 +55,9 @@ solve_task_astar(Task,Agenda,D,RR,Cost,NewPos) :-
   ( setof([c(F1,G1,P1)|RR1], search_astar(Task,Pos,F1,G,G1,P1,RPath,RR1), Children)
   -> append(Children,Rest,NewAgenda) ; NewAgenda = Rest),
   D1 is D+1,
-  solve_task_astar(Task,NewAgenda,D1,RR,Cost,NewPos).  % backtrack search
+  solve_task_astar(Task,NewAgenda,D1,RR,Cost,NewPos,Visited).  % backtrack search
 
-solve_task_astar(Task,Agenda,D,RR,Cost,NewPos) :-
+solve_task_astar(Task,Agenda,D,RR,Cost,NewPos,Visited) :-
   Task = find(O),
   % writeln('Solve astar find'),
   Agenda =  [[c(F,G,Pos)|RPath]|Rest],
@@ -66,9 +66,9 @@ solve_task_astar(Task,Agenda,D,RR,Cost,NewPos) :-
   % writeln('Printing Agenda'),
   % print_agenda(Agenda),
   ( setof([c(F1,G1,P1)|RR1], search_astar(Task,Pos,F1,G,G1,P1,RPath,RR1), Children)
-  -> append(Rest,Children,NewAgenda) ; NewAgenda = Rest),
+  -> remove_visited(Visited,Children,Children1), append(Rest,Children1,NewAgenda) ; NewAgenda = Rest),
   D1 is D+1,
-  solve_task_astar(Task,NewAgenda,D1,RR,Cost,NewPos).  % backtrack search
+  solve_task_astar(Task,NewAgenda,D1,RR,Cost,NewPos,Visited).  % backtrack search
 
 search_astar(go(P),Pos,F,G,G1,P1,RPath,NewRR) :-
   % writeln('Searching astar go'),
@@ -106,3 +106,12 @@ print_agenda(Agenda) :-
   Agenda =  [[c(F,G,Pos)|RPath]|Rest],
   write('F = '), write(F), write(' G = '), write(G), write(' Pos = '), write(Pos),write(' RPath'),writeln(RPath).
   print_agenda(Rest).
+
+remove_visited(Visited,NewVisited,[],NewChildren).
+
+remove_visited(Visited,NewVisited,Children,NewChildren) :-
+  Children = [[c(F,G,P)|RR]|RestChildren],
+  (\+ memberchk(P, Visited) -> NewVisited = [P1|Visited], NewChildren = Children
+    ; NewVisited = Visited, delete(Children, [c(F,G,P)|RR], NewChildren)
+  ),
+  remove_visited( RestChildren,
